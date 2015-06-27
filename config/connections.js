3f297094afdc81d352632ea9ea66ee12cab5493f
David Moritz <davidmoritz@gmail.com>
2015-06-27 06:30:05 -0500

    add mysql

--- config/connections.js
+++ config/connections.js
@@ -40,12 +40,9 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  localMySQL: {
-    adapter: 'sails-mysql',
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'ey'
+  mysql: {
+    module    : 'sails-mysql',
+    url       : 'mysql2://splendiduser:splended@splendid-gems.crw5t2eabd7d.us-west-2.rds.amazonaws.com:3306/splendid'
   },
 
   /***************************************************************************
